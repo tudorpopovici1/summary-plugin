ed0a17d05af4abe0bb322a08481c3dcc45b4375e
Tudor <t.popovici@student.tudelft.nl>
2020-04-10 02:49:58 +0300

    Initial commit

---
+++ src/main/java/actions/MethodSummaryAction.java
@@ -0,0 +1,19 @@
+package actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class MethodSummaryAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        throw new NotImplementedException();
+    }
+}
